524bb9cc203cc598eaa073b0ba7c9d3ce18cd986
Robert Koch <robert.koch@entelect.co.za>
2017-01-26 13:39:27 +0200

    hello world web app done, onto logging in!

---
+++ web/src/main/java/za/co/entelect/jbootcamp/web/RootController.java
@@ -0,0 +1,13 @@
+package za.co.entelect.jbootcamp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+    @RequestMapping("")
+    public String getSiteRoot() {
+        return "home";
+    }
+}
